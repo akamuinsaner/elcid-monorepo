9b96f3e1c513399c276e6b1623e31c3fcf44e569
akamuinsaner <15108387504@163.com>
2024-08-30 20:32:23 +0800

    add signin page

--- apps/account/src/app/signIn/page.tsx
+++ apps/account/src/app/signIn/page.tsx
@@ -1,57 +1,20 @@
 'use client';
-import {
-    Button,
-    Checkbox,
-    Form,
-    FormItem,
-    Input,
-    Link,
-} from '@elcid-monorepo/widgets';
-import {
-    RiCheckboxCircleFill,
-    RiEyeCloseLine,
-    RiEyeLine,
-} from '@remixicon/react';
+import { MESSAGE, REGEX } from '@elcid-monorepo/constants';
+import { Button, Form, FormItem, Input, Link } from '@elcid-monorepo/widgets';
+import { RiEyeCloseLine, RiEyeLine } from '@remixicon/react';
 import { useForm } from 'packages/widgets/src/lib/Form/useForm';
-import { memo, useCallback, useRef, useState } from 'react';
-import { REGEX, MESSAGE } from '@elcid-monorepo/constants';
-import { twMerge } from 'tailwind-merge';
-import classNames from 'classnames';
+import { useCallback, useRef, useState } from 'react';
 
-const PASSWORD_RULES = [
-    {
-        rule: '8 - 64 characters ',
-        validator: (pwd: string) => /^.{8,64}$/.test(pwd),
-    },
-    {
-        rule: 'One uppercase letter',
-        validator: (pwd: string) => /^[A-Z]$/.test(pwd),
-    },
-    {
-        rule: 'One lowercase letter',
-        validator: (pwd: string) => /^[a-z]$/.test(pwd),
-    },
-    { rule: 'One number', validator: (pwd: string) => /^[0-9]$/.test(pwd) },
-    {
-        rule: 'One special character (e.g., ! @ # $ % ^ & *)',
-        validator: (pwd: string) => /^[\!\@\#\$\%\^\&\*]$/.test(pwd),
-    },
-];
-
-const SignUp = () => {
+const SignIn = () => {
     const [pwdVisible, setPwdVisible] = useState<boolean>(false);
-    const [agreed, setAgreed] = useState<boolean>(false);
-    const [password, setPassword] = useState<string>('');
     const form = useRef(useForm());
 
     const onSubmit = useCallback(() => {
-        if (!agreed) return;
         form.current.validates((errors, values) => {
             if (errors) return;
             console.log(values);
         });
-    }, [agreed]);
-
+    }, []);
     return (
         <div className='flex-1 flex flex-col justify-center items-center px-3 w-full  mx-auto desktop:flex-row desktop:gap-8 desktop:px-24 desktop:py-8'>
             <div className='gap-6 flex-1 flex flex-col justify-center w-full tablet:w-[456px] desktop:h-[672px] desktop:px-[102px] desktop:flex-none desktop:w-[592px]'>
@@ -59,15 +22,9 @@ const SignUp = () => {
                     role='heading'
                     className='text-3xl font-semibold text-primary '
                 >
-                    Create your account
+                    Log in to your account
                 </h3>
-                <Form
-                    className='flex flex-col gap-6'
-                    form={form.current}
-                    onValuesChange={(_, cur) => {
-                        setPassword(cur.password || '');
-                    }}
-                >
+                <Form className='flex flex-col gap-6' form={form.current}>
                     <FormItem
                         name='email'
                         label='Email'
@@ -113,68 +70,31 @@ const SignUp = () => {
                         />
                     </FormItem>
                 </Form>
-                <ul className='flex flex-col gap-2'>
-                    {PASSWORD_RULES.map(rule => {
-                        const classes = twMerge(
-                            'w-5 h-5 text-tertiary',
-                            classNames({
-                                'text-success': rule.validator(password),
-                            }),
-                        );
-                        return (
-                            <li
-                                className='flex gap-3 items-center'
-                                key={rule.rule}
-                            >
-                                <RiCheckboxCircleFill className={classes} />
-                                <span className='text-xs text-secondary'>
-                                    {rule.rule}
-                                </span>
-                            </li>
-                        );
-                    })}
-                </ul>
-                <div className='flex items-center gap-3'>
-                    <Checkbox
-                        checked={agreed}
-                        onChange={e => setAgreed(e.target.checked)}
-                    />
-                    <span className='text-xs text-secondary'>
-                        I agree with CodePulse{' '}
-                        <Link
-                            className='text-xs text-brand hover:text-brand'
-                            name='Terms of Service'
-                            aria-label='Terms of Service'
-                        >
-                            Terms of Service
-                        </Link>
-                    </span>
-                </div>
                 <Button
                     className='w-full'
-                    name='Create account'
-                    aria-label='Create account'
+                    name='Submit'
+                    aria-label='Submit'
                     size='sm'
                     onClick={onSubmit}
                 >
-                    Create account
+                    Submit
                 </Button>
                 <div className='text-center text-xs font-medium text-primary'>
-                    Already have an account?{' '}
+                    Don’t have an account?{' '}
                     <Link
                         className='text-xs text-brand hover:text-brand'
                         name='Sign in'
                         aria-label='Sign in'
-                        href='/signIn'
+                        href='/signUp'
                     >
-                        Sign in
+                        Sign up
                     </Link>{' '}
                 </div>
             </div>
             <div className='hidden desktop:block flex-1 h-[672px]'>
                 <img
-                    alt='signup'
-                    src='/signup.webp'
+                    alt='signIn'
+                    src='/signIn.webp'
                     className='w-full h-full object-center object-cover'
                     loading='lazy'
                 />
@@ -183,4 +103,4 @@ const SignUp = () => {
     );
 };
 
-export default memo(SignUp);
+export default SignIn;
